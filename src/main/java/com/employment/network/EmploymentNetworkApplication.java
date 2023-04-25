3dca0e6aa2660220221ccbc8bfb001235a45f118
narayanram-n2r <narayanramnarayanan@gmail.com>
2023-04-24 23:07:22 -0400

    Init Commit - Maven Dependencies.

---
+++ src/main/java/com/employment/network/EmploymentNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.employment.network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploymentNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmploymentNetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/employment/network/EmploymentNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employment.network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmploymentNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
